0c5650fce39329c2891c10b97d6f65b5436925dd
sha-opt <shadow_opt@sina.com>
2026-03-20 20:36:49 +0800

    fix win

--- legged_gym/envs/nocv/win_cfg.py
+++ legged_gym/envs/nocv/win_cfg.py
@@ -11,8 +11,8 @@ class WINCfg(GO2Cfg):
         file = '{LEGGED_GYM_ROOT_DIR}/resources/robots/j60/urdf/z2.urdf'
         name = 'z2'
         foot_name = 'FOOT'
-        terminate_after_contacts_on = ["base","trunk"]
-        penalize_contacts_on = ["thigh", "calf", "hip", "base"]
+        terminate_after_contacts_on = ["base"]
+        penalize_contacts_on = ["thigh", "calf", "hip"]
         self_collisions = 1 # 1关闭自碰撞，0开启自碰撞
         flip_visual_attachments = True
 
