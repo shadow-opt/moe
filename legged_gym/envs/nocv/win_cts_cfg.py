76dcaf04b94d6a549a1508838d694f2d8400ad0b
sha-opt <shadow_opt@sina.com>
2026-03-29 08:50:42 +0800

    vanilla

--- legged_gym/envs/nocv/win_cts_cfg.py
+++ legged_gym/envs/nocv/win_cts_cfg.py
@@ -80,6 +80,10 @@ class WINVanillaCTS(WINCTS):
         special_terrain_command = 0
         normal_terrain_command = 0
         special_terrain_command_obs_scale = 1.0
+    class terrain(WINCTS.terrain):
+        terrain_proportions = [0.05, 0.05, 0.1, 0.3, 0.1, 0.2, 0.0, 0.0, 0.2]
+        # [wave, slope, rough_slope, stairs up, stairs down, obstacles, stones, gap, flat]
+
 
 
 
