c54d82a53d396f346431e7f9cf17834fc1a85bcd
sha-opt <shadow_opt@sina.com>
2026-06-03 18:36:50 +0800

    foot_slip off

--- legged_gym/envs/nocv/win_cfg.py
+++ legged_gym/envs/nocv/win_cfg.py
@@ -162,8 +162,8 @@ class WINCfg(GO2Cfg):
         curriculum_rewards = [
             {'reward_name': 'lin_vel_z', 'start_iter': 0, 'end_iter': 1500, 'start_value': 1.0, 'end_value': 0.0},
             {'reward_name': 'correct_base_height', 'start_iter': 0, 'end_iter': 5000, 'start_value': 1.0, 'end_value': 10.0},
-            {'reward_name': 'ang_vel_xy', 'start_iter': 0, 'end_iter': 70000, 'start_value': 1.0, 'end_value': 2.0},
-            {'reward_name': 'foot_slip', 'start_iter': 30000, 'end_iter': 60000, 'start_value': 1.0, 'end_value': 3.0},
+            {'reward_name': 'ang_vel_xy', 'start_iter': 10000, 'end_iter': 30000, 'start_value': 1.0, 'end_value': 2.0},
+            # {'reward_name': 'foot_slip', 'start_iter': 30000, 'end_iter': 60000, 'start_value': 1.0, 'end_value': 2.0},
             # {'reward_name': 'x_command_hip_regular', 'start_iter': 30000, 'end_iter': 60000, 'start_value': 1.0, 'end_value': 10.0},
             {'reward_name': 'stand_still', 'start_iter': 10000, 'end_iter': 40000, 'start_value': 1.0, 'end_value': 5.0},
             {'reward_name': 'hip_to_default', 'start_iter': 20000, 'end_iter': 70000, 'start_value': 1.0, 'end_value': 0.4},
@@ -184,12 +184,12 @@ class WINCfg(GO2Cfg):
         class scales(GO2Cfg.rewards.scales):
             # straight_path = 10.0 # [NOTE] 新增
             # straight_path_deviation = -3 # [NOTE] 新增
-            ang_vel_xy = -0.07
+            ang_vel_xy = -0.06
             lateral_yaw_tracking_error = -0.6
             stand_still = -1.0
             action_smoothness = -0.01
-            foot_slip = -0.03
-            hip_to_zero = -0.2
+            # foot_slip = -0.03
+            hip_to_zero = -0.5
                 
             # orientation = -0.1
             # stumble = -0.5
